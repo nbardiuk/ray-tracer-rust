bf59319e16754737b32af3bbb868f11d323b7f5d
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-02-09 17:38:27 +0000

    add basic tuple arithmetics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::ops::Add;
+use std::ops::Mul;
+use std::ops::Div;
+use std::ops::Neg;
+use std::ops::Sub;
+
 #[derive(Debug, PartialEq)]
 struct Tuple {
     x: f64,
@@ -12,6 +18,70 @@ impl Tuple {
     }
     fn is_vector(&self) -> bool {
         self.w == 0.0
+    }
+}
+
+impl Add for Tuple {
+    type Output = Tuple;
+
+    fn add(self, other: Tuple) -> Tuple {
+        Tuple {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
+    }
+}
+
+impl Sub for Tuple {
+    type Output = Tuple;
+
+    fn sub(self, other: Tuple) -> Tuple {
+        Tuple {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+            w: self.w - other.w,
+        }
+    }
+}
+
+impl Neg for Tuple {
+    type Output = Tuple;
+
+    fn neg(self) -> Tuple {
+        Tuple {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+            w: -self.w,
+        }
+    }
+}
+
+impl Mul<f64> for Tuple {
+    type Output = Tuple;
+
+    fn mul(self, other: f64) -> Tuple {
+        Tuple {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
+            w: self.w * other,
+        }
+    }
+}
+impl Div<f64> for Tuple {
+    type Output = Tuple;
+
+    fn div(self, other: f64) -> Tuple {
+        Tuple {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
+            w: self.w / other,
+        }
     }
 }
 
@@ -30,7 +100,7 @@ fn main() {
 }
 
 #[cfg(test)]
-mod tests {
+mod tuples {
     use point;
     use tuple;
     use vector;
@@ -67,5 +137,64 @@ mod tests {
     fn vector_creates_tuple_with_w0() {
         let v = vector(4.0, -4.0, 3.0);
         assert_eq!(v, tuple(4.0, -4.0, 3.0, 0.0));
+    }
+
+    #[test]
+    fn adding_two_tuples() {
+        let a1 = tuple(3.0, -2.0, 5.0, 1.0);
+        let a2 = tuple(-2.0, 3.0, 1.0, 0.0);
+        assert_eq!(a1 + a2, tuple(1.0, 1.0, 6.0, 1.0));
+    }
+
+    #[test]
+    fn subtracting_two_tuples() {
+        let p1 = point(3.0, 2.0, 1.0);
+        let p2 = point(5.0, 6.0, 7.0);
+        assert_eq!(p1 - p2, vector(-2.0, -4.0, -6.0));
+    }
+
+    #[test]
+    fn subtracting_vector_from_a_point() {
+        let p = point(3.0, 2.0, 1.0);
+        let v = vector(5.0, 6.0, 7.0);
+        assert_eq!(p - v, point(-2.0, -4.0, -6.0));
+    }
+
+    #[test]
+    fn subtracting_two_vectors() {
+        let v1 = vector(3.0, 2.0, 1.0);
+        let v2 = vector(5.0, 6.0, 7.0);
+        assert_eq!(v1 - v2, vector(-2.0, -4.0, -6.0));
+    }
+
+    #[test]
+    fn subtracting_a_vector_from_the_zero_vector() {
+        let zero = vector(0.0, 0.0, 0.0);
+        let v = vector(1.0, -2.0, 3.0);
+        assert_eq!(zero - v, vector(-1.0, 2.0, -3.0));
+    }
+
+    #[test]
+    fn negating_a_tuple() {
+        let a = tuple(1.0, -2.0, 3.0, -4.0);
+        assert_eq!(-a, tuple(-1.0, 2.0, -3.0, 4.0));
+    }
+
+    #[test]
+    fn multiplying_a_tuple_by_a_scalar() {
+        let a = tuple(1.0, -2.0, 3.0, -4.0);
+        assert_eq!(a * 3.5, tuple(3.5, -7.0, 10.5, -14.0));
+    }
+
+    #[test]
+    fn multiplying_a_tuple_by_a_fraction() {
+        let a = tuple(1.0, -2.0, 3.0, -4.0);
+        assert_eq!(a * 0.5, tuple(0.5, -1.0, 1.5, -2.0));
+    }
+
+    #[test]
+    fn dividing_a_tuple_by_a_scalar() {
+        let a = tuple(1.0, -2.0, 3.0, -4.0);
+        assert_eq!(a / 2.0, tuple(0.5, -1.0, 1.5, -2.0));
     }
 }
