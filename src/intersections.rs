5e2c98bc226bbd62853cd6ae1136bf5c41c742e4
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-02-10 23:07:53 +0000

    add hit intersection

--- src/intersections.rs
+++ src/intersections.rs
@@ -1,4 +1,4 @@
-#[derive(Clone)]
+#[derive(Debug, PartialEq)]
 pub struct Intersection<'a, T> {
     pub t: f64,
     pub object: &'a T,
@@ -13,6 +13,12 @@ pub fn intersections<'a, T>(
     b: Intersection<'a, T>,
 ) -> Vec<Intersection<'a, T>> {
     vec![a, b]
+}
+
+pub fn hit<'a, T>(xs: &'a Vec<Intersection<'a, T>>) -> Option<&'a Intersection<'a, T>> {
+    xs.iter()
+        .filter(|x| x.t >= 0.)
+        .min_by(|a, b| a.t.partial_cmp(&b.t).unwrap())
 }
 
 #[cfg(test)]
@@ -39,5 +45,47 @@ mod spec {
         assert_eq!(xs.len(), 2);
         assert_eq!(xs[0].t, 1.);
         assert_eq!(xs[1].t, 2.);
+    }
+
+    #[test]
+    fn the_hit_when_all_intersections_have_positive_t() {
+        let s = sphere();
+        let i1 = intersection(1., &s);
+        let i2 = intersection(2., &s);
+        let xs = intersections(i1, i2);
+
+        assert_eq!(hit(&xs).unwrap(), &intersection(1., &s));
+    }
+
+    #[test]
+    fn the_hit_when_some_intersections_have_negative_t() {
+        let s = sphere();
+        let i1 = intersection(-1., &s);
+        let i2 = intersection(1., &s);
+        let xs = intersections(i2, i1);
+
+        assert_eq!(hit(&xs).unwrap(), &intersection(1., &s));
+    }
+
+    #[test]
+    fn the_hit_when_all_intersections_have_negative_t() {
+        let s = sphere();
+        let i1 = intersection(-2., &s);
+        let i2 = intersection(-1., &s);
+        let xs = intersections(i1, i2);
+
+        assert_eq!(hit(&xs), None);
+    }
+
+    #[test]
+    fn the_hit_is_always_the_lowest_non_negative_intersection() {
+        let s = sphere();
+        let i1 = intersection(5., &s);
+        let i2 = intersection(7., &s);
+        let i3 = intersection(-3., &s);
+        let i4 = intersection(2., &s);
+        let xs = &vec![i1, i2, i3, i4];
+
+        assert_eq!(hit(&xs).unwrap(), &intersection(2., &s));
     }
 }
