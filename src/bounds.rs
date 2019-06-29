f4fddc4edc931cb9197b57075b79e3131ef1c27c
Nazarii Bardiuk <nazarii@bardiuk.com>
2019-06-29 14:59:29 +0100

    bounds intersection

--- src/bounds.rs
+++ src/bounds.rs
@@ -1,4 +1,8 @@
+use intersections::EPSILON;
 use matrices::Matrix;
+use rays::Ray;
+use std::f64::INFINITY;
+use std::f64::NEG_INFINITY;
 use std::ops::Add;
 use tuples::point;
 use tuples::Tuple;
@@ -16,7 +20,32 @@ pub fn bound_single(p: Tuple) -> Bounds {
     bound(p.clone(), p.clone())
 }
 
+fn check_axis(origin: f64, direction: f64, minimum: f64, maximum: f64) -> (f64, f64) {
+    let tmin_numerator = minimum - origin;
+    let tmax_numerator = maximum - origin;
+    let (tmin, tmax) = (tmin_numerator / direction, tmax_numerator / direction);
+    if tmin > tmax {
+        (tmax, tmin)
+    } else {
+        (tmin, tmax)
+    }
+}
 impl Bounds {
+    pub fn intersects(&self, ray: &Ray) -> bool {
+        let (xtmin, xtmax) = check_axis(ray.origin.x, ray.direction.x, self.min.x, self.max.x);
+        let (ytmin, ytmax) = check_axis(ray.origin.y, ray.direction.y, self.min.y, self.max.y);
+        let (ztmin, ztmax) = check_axis(ray.origin.z, ray.direction.z, self.min.z, self.max.z);
+        let tmin = vec![xtmin, ytmin, ztmin]
+            .into_iter()
+            .max_by(|a, b| a.partial_cmp(&b).unwrap())
+            .unwrap();
+        let tmax = vec![xtmax, ytmax, ztmax]
+            .into_iter()
+            .min_by(|a, b| a.partial_cmp(&b).unwrap())
+            .unwrap();
+
+        tmin <= tmax && (tmin >= 0. || tmax >= 0.)
+    }
     pub fn transform(&self, transform: &Matrix) -> Bounds {
         let bounds: Vec<Bounds> = vec![
             point(self.min.x, self.min.y, self.min.z),
@@ -61,10 +90,12 @@ impl Add for Bounds {
 #[cfg(test)]
 mod spec {
     use super::*;
+    use rays::ray;
     use std::f64::consts::PI;
     use transformations::rotation_x;
     use transformations::rotation_y;
     use transformations::rotation_z;
+    use tuples::vector;
 
     #[test]
     fn bound_is_a_pair_of_points() {
@@ -102,5 +133,28 @@ mod spec {
             square.transform(&rotation_z(on_edge)),
             bound(point(-sq2, -sq2, -1.), point(sq2, sq2, 1.))
         );
+    }
+    #[test]
+    fn bounds_intersection() {
+        let b = bound(point(-1., -1., -1.), point(1., 1., 1.));
+        for (origin, direction, intersects) in vec![
+            (point(0., 0., 0.), vector(1., 1., 1.), true), // inside diagonal
+            (point(2., 2., 2.), vector(1., 1., 1.), false), // outside out
+            (point(0., 2., 0.), vector(0., -1., 0.), true), // outside in
+            (point(0., 2., 0.), vector(-1., -1., 0.), true), // outside in
+            (point(0., 1.1, 0.), vector(1., 1., 0.), false), // outside parallel
+            (point(0., 1.1, 0.), vector(0., 1., 1.), false), // outside parallel
+            (point(0., 1.1, 0.), vector(1., 0., 1.), false), // outside parallel
+        ] {
+            let r = ray(origin.clone(), direction.clone());
+
+            assert_eq!(
+                b.intersects(&r),
+                intersects,
+                "where ray {:?} {:?}",
+                origin,
+                direction
+            );
+        }
     }
 }
